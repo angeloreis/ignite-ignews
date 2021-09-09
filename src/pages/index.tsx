f2724189379489634767de42eb8279e023ebdb11
Angelo Reis <angelo.reis@kumulus.com.br>
2021-09-09 17:36:02 -0300

    feat: create interface props and ssg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,21 @@
 /* eslint-disable @next/next/no-img-element */
+import { GetStaticProps } from 'next'
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
+import { formatPrice } from '../utils/format'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number,
+  }
+}
+
+export default function Home({ product }: HomeProps) {
+  
   return (
     <>
     <Head>
@@ -17,9 +28,9 @@ export default function Home() {
         <h1>News about the <span>React</span> world.</h1>
         <p>
           Get access to all publications <br/>
-          <span>for $9.99/month</span>
+          <span>for {product.amount}/month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId}/>
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding" />
@@ -27,4 +38,20 @@ export default function Home() {
     
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1JXsszFg4cUTGbILbqsjbqmi')
+
+  const product = {
+    priceId: price.id,
+    amount: formatPrice((price.unit_amount / 100)),
+  }
+
+  return {
+    props: {  
+      product,
+    },
+    revalidate: 60 * 60 * 24, //24 hours
+  }
 }
