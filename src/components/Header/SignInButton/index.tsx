69e64e93a4b2f7b3794988a028b09e0f1a1508ab
Angelo Reis <angelo.reis@kumulus.com.br>
2021-09-20 16:08:50 -0300

    fix: nextauth config nad button with signin and ou method

--- src/components/Header/SignInButton/index.tsx
+++ src/components/Header/SignInButton/index.tsx
@@ -1,22 +1,27 @@
+import {signIn, signOut, useSession} from 'next-auth/client'
 import { FaGithub } from 'react-icons/fa'
 import { FiX } from 'react-icons/fi'
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-    const isUserLoggedIn = false;
+    const [session] = useSession()
 
-    const colorIconGitHub = isUserLoggedIn ? "#04d361" : "#eba417";
+    const colorIconGitHub = session ? "#04d361" : "#eba417";
+    const contentTextButton = session ? session.user.name : "Sign in with Github";
 
-    const contentTextButton = isUserLoggedIn ? "Angelo Reis" : "Sign in with Github";
+    const authOnApp = () => {
+        session ? signOut() : signIn('github')
+    }
 
     return (
         <button
          type="button"
-         className={styles.signInButton}>
+         className={styles.signInButton}
+         onClick={authOnApp}>
             <FaGithub color={colorIconGitHub}/>
             {contentTextButton}
-            {isUserLoggedIn && <FiX className={styles.closeIcon} color="#737380"/>}
+            {session && <FiX className={styles.closeIcon} color="#737380"/>}
         </button>
     );
 }
